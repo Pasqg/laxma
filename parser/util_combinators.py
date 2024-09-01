fb0a2995bc733c65e948dfc6e797b4680d415de5
Pasqg <pasquale.giovenale@gmail.com>
2024-09-01 10:39:50 +0200

    Add ref combinator for recursive rules

--- parser/util_combinators.py
+++ parser/util_combinators.py
@@ -13,3 +13,15 @@ def discard(combinator: Combinator[RuleId, TokenType]) -> Combinator[RuleId, Tok
         return result, AST(), remaining
 
     return inner
+
+
+def ref(combinator: Combinator[RuleId, TokenType]) -> Combinator[RuleId, TokenType]:
+    """
+        Adds a level of indirection like a reference to allow self/mutual recursive definitions.
+        Example: combinator2 = orMath(ref(lambda t: combinator1(t)), match_none)
+    """
+
+    def inner(tokens):
+        return combinator(tokens)
+
+    return inner
